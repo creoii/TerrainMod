f9b4178aafd5f08848cc31712755594f0ae61f48
creoii <33011337+creoii@users.noreply.github.com>
2021-10-31 21:27:11 -0400

    loot tables and update to 21w43a

--- src/main/java/creoii/terrain/block/CrystalFlowerBlock.java
+++ src/main/java/creoii/terrain/block/CrystalFlowerBlock.java
@@ -2,6 +2,7 @@ package creoii.terrain.block;
 
 import net.minecraft.block.*;
 import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.class_6760;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
@@ -57,7 +58,7 @@ public class CrystalFlowerBlock extends Block implements Waterloggable {
     @SuppressWarnings("deprecation")
     public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
         if (state.get(WATERLOGGED)) {
-            world.getFluidTickScheduler().schedule(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+            world.getFluidTickScheduler().method_39363(class_6760.method_39411(Fluids.WATER, pos, Fluids.WATER.getTickRate(world)));
         }
 
         return direction == (state.get(FACING)).getOpposite() && !state.canPlaceAt(world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
